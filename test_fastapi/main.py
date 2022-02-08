0e1c7e130d90b32a1249f418d339c565374133a9
한송희 <98_han@naver.com>
2022-02-08 20:58:09 +0900

    using RealDictCursor convert datatype from list to dictonary

--- test_fastapi/main.py
+++ test_fastapi/main.py
@@ -1,9 +1,8 @@
 from typing import Optional
-
 from fastapi import FastAPI
 from pydantic import BaseModel
-
 import psycopg2
+import psycopg2.extras
 
 
 app = FastAPI()
@@ -20,22 +19,29 @@ sslmode = "require"
 conn_string = "host={0} user={1} dbname={2} password={3} sslmode={4}".format(host, user, dbname, password, sslmode)
 conn = psycopg2.connect(conn_string)
 print("Connection established")
-cursor = conn.cursor()
+cursor = conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
 
-class Item(BaseModel):
+class Fruit(BaseModel):
+    id: int
     name: str
-    price: float
+    season: str
+    description: str
     is_offer: Optional[bool] = None
-
 
 @app.get("/")
 def read_root():
     cursor.execute("SELECT * FROM public.fruits1;")
     rows = cursor.fetchall()
-    # Print all rows
+    return rows
 
-    fruitsArray = []
-    for item in rows:
-        fruitsArray.append(rows)
-        return fruitsArray
+    for fruit in rows:
+        fruit.append(dict(rows))
+        return fruit
 
+@app.get("/fruits/{fruit_id}")
+def read_fruit(fruit_id: int, fr, q: Optional[str] = None):
+    return {"fruit_id": fruit_id, "q": q}
+
+@app.put("/fruits/{fruit_id}")
+def update_fruit(fruit_id: int, fruit: Fruit):
+    return {"fruit_id": fruit.id, "fruit_name": fruit.name, "fruit_season": fruit.season, "fruit_description": fruit.description}
