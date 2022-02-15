d525eceb7e97dfcc806c83c351a93e27071d110f
한송희 <98_han@naver.com>
2022-02-15 20:34:57 +0900

    Update HTTP Request

--- test_fastapi/main.py
+++ test_fastapi/main.py
@@ -19,7 +19,7 @@ sslmode = "require"
 conn_string = "host={0} user={1} dbname={2} password={3} sslmode={4}".format(host, user, dbname, password, sslmode)
 conn = psycopg2.connect(conn_string)
 print("Connection established")
-cursor = conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+cursor = conn.cursor()
 
 class Fruit(BaseModel):
     id: int
@@ -28,20 +28,28 @@ class Fruit(BaseModel):
     description: str
     is_offer: Optional[bool] = None
 
+
 @app.get("/")
 def read_root():
-    cursor.execute("SELECT * FROM public.fruits1;")
-    rows = cursor.fetchall()
-    return rows
+    cursor.execute("Select * FROM public.fruits1 LIMIT 0")
+    colnames = [desc[0] for desc in cursor.description]
 
-    for fruit in rows:
-        fruit.append(dict(rows))
-        return fruit
+    cursor.execute("SELECT * FROM public.fruits1;")
+
+    fruit = []
+    for row in cursor.fetchall():
+        fruit.append(dict(zip(colnames, row)))
+    return fruit
 
 @app.get("/fruits/{fruit_id}")
-def read_fruit(fruit_id: int, fr, q: Optional[str] = None):
-    return {"fruit_id": fruit_id, "q": q}
+def read_fruit(fruit_id: int):
+    cursor.execute("Select * FROM public.fruits1 LIMIT 0")
+    colnames = [desc[0] for desc in cursor.description]
 
-@app.put("/fruits/{fruit_id}")
-def update_fruit(fruit_id: int, fruit: Fruit):
-    return {"fruit_id": fruit.id, "fruit_name": fruit.name, "fruit_season": fruit.season, "fruit_description": fruit.description}
+    cursor.execute(f"SELECT * FROM public.fruits1 WHERE id={fruit_id}")
+
+    fruit = []
+    for row in cursor.fetchall():
+        fruit.append(dict(zip(colnames, row)))
+    return fruit
+
